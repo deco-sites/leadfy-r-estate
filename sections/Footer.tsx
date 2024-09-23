24662920d618d92755fe218b68f960bf889181aa
Gabriel Collo <gcollo46@gmail.com>
2024-09-23 08:35:00 -0300

    style: copy text color

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -211,7 +211,7 @@ export default function Footer({
             ))}
           </div>
         </div>
-        <div class="border-primary border-t flex flex-col gap-4 items-center justify-between lg:flex-row lg:items-center py-8">
+        <div class="border-primary border-t flex flex-col gap-4 items-center justify-between lg:flex-row lg:items-center py-8 text-[#6D737A]">
           <div class="flex flex-col gap-4 items-center lg:flex-row lg:gap-6">
             <a
               href={madeWith?.href}
