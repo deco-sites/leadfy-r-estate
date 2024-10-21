2539633be6ec51a8e7447f4de01c3ab30675b1e3
André Perotto <andre@leadfy.me>
2024-10-21 20:48:15 +0000

    Publish 2 from stage into origin/main

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -7,34 +7,33 @@ import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$0 from "./loaders/propertiesData.ts";
 import * as $$$$$$0 from "./sections/AboutUsBody.tsx";
 import * as $$$$$$1 from "./sections/AdvenceSearch.tsx";
-import * as $$$$$$26 from "./sections/banner-leadfy.tsx";
+import * as $$$$$$25 from "./sections/banner-leadfy.tsx";
 import * as $$$$$$2 from "./sections/BannerAboutUs.tsx";
 import * as $$$$$$3 from "./sections/BannerCategory.tsx";
 import * as $$$$$$4 from "./sections/BlogPosts.tsx";
 import * as $$$$$$5 from "./sections/Faq.tsx";
 import * as $$$$$$6 from "./sections/FeaturedProperty.tsx";
 import * as $$$$$$7 from "./sections/Footer.tsx";
-import * as $$$$$$10 from "./sections/GlobalTags.tsx";
-import * as $$$$$$8 from "./sections/GTM.tsx";
-import * as $$$$$$9 from "./sections/GTMSection.tsx";
-import * as $$$$$$11 from "./sections/Header.tsx";
-import * as $$$$$$12 from "./sections/HeaderReal.tsx";
-import * as $$$$$$27 from "./sections/hero-1.tsx";
-import * as $$$$$$28 from "./sections/hero-2.tsx";
-import * as $$$$$$13 from "./sections/Hero.tsx";
-import * as $$$$$$29 from "./sections/hero2.tsx";
-import * as $$$$$$14 from "./sections/HeroBanner.tsx";
-import * as $$$$$$15 from "./sections/HeroProductPage.tsx";
-import * as $$$$$$16 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$17 from "./sections/Logos.tsx";
-import * as $$$$$$18 from "./sections/PropertyBuyOption.tsx";
-import * as $$$$$$19 from "./sections/PropertyDescription.tsx";
-import * as $$$$$$20 from "./sections/PropertyLocation.tsx";
-import * as $$$$$$21 from "./sections/PropertyOverview.tsx";
-import * as $$$$$$22 from "./sections/Shelf.tsx";
-import * as $$$$$$23 from "./sections/Testimonials.tsx";
-import * as $$$$$$24 from "./sections/Theme/Theme.tsx";
-import * as $$$$$$25 from "./sections/TitleSection.tsx";
+import * as $$$$$$9 from "./sections/GlobalTags.tsx";
+import * as $$$$$$8 from "./sections/GTMSection.tsx";
+import * as $$$$$$10 from "./sections/Header.tsx";
+import * as $$$$$$11 from "./sections/HeaderReal.tsx";
+import * as $$$$$$26 from "./sections/hero-1.tsx";
+import * as $$$$$$27 from "./sections/hero-2.tsx";
+import * as $$$$$$12 from "./sections/Hero.tsx";
+import * as $$$$$$28 from "./sections/hero2.tsx";
+import * as $$$$$$13 from "./sections/HeroBanner.tsx";
+import * as $$$$$$14 from "./sections/HeroProductPage.tsx";
+import * as $$$$$$15 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$16 from "./sections/Logos.tsx";
+import * as $$$$$$17 from "./sections/PropertyBuyOption.tsx";
+import * as $$$$$$18 from "./sections/PropertyDescription.tsx";
+import * as $$$$$$19 from "./sections/PropertyLocation.tsx";
+import * as $$$$$$20 from "./sections/PropertyOverview.tsx";
+import * as $$$$$$21 from "./sections/Shelf.tsx";
+import * as $$$$$$22 from "./sections/Testimonials.tsx";
+import * as $$$$$$23 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$24 from "./sections/TitleSection.tsx";
 
 const manifest = {
   "loaders": {
@@ -43,34 +42,33 @@ const manifest = {
   "sections": {
     "site/sections/AboutUsBody.tsx": $$$$$$0,
     "site/sections/AdvenceSearch.tsx": $$$$$$1,
-    "site/sections/banner-leadfy.tsx": $$$$$$26,
+    "site/sections/banner-leadfy.tsx": $$$$$$25,
     "site/sections/BannerAboutUs.tsx": $$$$$$2,
     "site/sections/BannerCategory.tsx": $$$$$$3,
     "site/sections/BlogPosts.tsx": $$$$$$4,
     "site/sections/Faq.tsx": $$$$$$5,
     "site/sections/FeaturedProperty.tsx": $$$$$$6,
     "site/sections/Footer.tsx": $$$$$$7,
-    "site/sections/GlobalTags.tsx": $$$$$$10,
-    "site/sections/GTM.tsx": $$$$$$8,
-    "site/sections/GTMSection.tsx": $$$$$$9,
-    "site/sections/Header.tsx": $$$$$$11,
-    "site/sections/HeaderReal.tsx": $$$$$$12,
-    "site/sections/hero-1.tsx": $$$$$$27,
-    "site/sections/hero-2.tsx": $$$$$$28,
-    "site/sections/Hero.tsx": $$$$$$13,
-    "site/sections/hero2.tsx": $$$$$$29,
-    "site/sections/HeroBanner.tsx": $$$$$$14,
-    "site/sections/HeroProductPage.tsx": $$$$$$15,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$16,
-    "site/sections/Logos.tsx": $$$$$$17,
-    "site/sections/PropertyBuyOption.tsx": $$$$$$18,
-    "site/sections/PropertyDescription.tsx": $$$$$$19,
-    "site/sections/PropertyLocation.tsx": $$$$$$20,
-    "site/sections/PropertyOverview.tsx": $$$$$$21,
-    "site/sections/Shelf.tsx": $$$$$$22,
-    "site/sections/Testimonials.tsx": $$$$$$23,
-    "site/sections/Theme/Theme.tsx": $$$$$$24,
-    "site/sections/TitleSection.tsx": $$$$$$25,
+    "site/sections/GlobalTags.tsx": $$$$$$9,
+    "site/sections/GTMSection.tsx": $$$$$$8,
+    "site/sections/Header.tsx": $$$$$$10,
+    "site/sections/HeaderReal.tsx": $$$$$$11,
+    "site/sections/hero-1.tsx": $$$$$$26,
+    "site/sections/hero-2.tsx": $$$$$$27,
+    "site/sections/Hero.tsx": $$$$$$12,
+    "site/sections/hero2.tsx": $$$$$$28,
+    "site/sections/HeroBanner.tsx": $$$$$$13,
+    "site/sections/HeroProductPage.tsx": $$$$$$14,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$15,
+    "site/sections/Logos.tsx": $$$$$$16,
+    "site/sections/PropertyBuyOption.tsx": $$$$$$17,
+    "site/sections/PropertyDescription.tsx": $$$$$$18,
+    "site/sections/PropertyLocation.tsx": $$$$$$19,
+    "site/sections/PropertyOverview.tsx": $$$$$$20,
+    "site/sections/Shelf.tsx": $$$$$$21,
+    "site/sections/Testimonials.tsx": $$$$$$22,
+    "site/sections/Theme/Theme.tsx": $$$$$$23,
+    "site/sections/TitleSection.tsx": $$$$$$24,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
