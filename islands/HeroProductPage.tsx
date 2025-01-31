1c8aad28272b91c6ad161ca5eb713919d8ed2ca7
André Perotto <138815928+andreperotto@users.noreply.github.com>
2025-01-31 15:46:40 -0300

    Update HeroProductPage.tsx
    ajustando mobile

--- islands/HeroProductPage.tsx
+++ islands/HeroProductPage.tsx
@@ -66,7 +66,7 @@ function GalleryProductPage({ images }: { images: string[] }) {
   const height = 764;
 
   return (
-    <div id={id} className="w-full md:w-2/3 max-w-[1000px] relative">
+    <div id={id} className="w-full md:w-2/3 max-w-[1000px] relative flex justify-center">
       <Slider className="carousel carousel-center w-full overflow-hidden border-none">
         {images.map((image, index) => (
           <Slider.Item
@@ -76,7 +76,7 @@ function GalleryProductPage({ images }: { images: string[] }) {
           >
             <ZoomImage>
               <img
-                className="w-full h-auto md:max-h-[500px] max-h-[75vh] object-contain border-none"
+                className="w-full h-auto max-h-[90vh] md:max-h-[500px] object-contain border-none"
                 src={image}
                 width={width}
                 height={height}
